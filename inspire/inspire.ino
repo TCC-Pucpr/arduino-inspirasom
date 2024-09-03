04e154311117b44c16806b920c79d30d99ba4966
KnightLeo <knightleo602@gmail.com>
2024-09-02 22:49:51 -0300

    Revert "ajustes"
    This reverts commit 192cd9eadf2ffd09052f47671d83dc56815590d6.

--- inspire/inspire.ino
+++ inspire/inspire.ino
@@ -1,18 +1,12 @@
 #include <Control_Surface.h>
 #include "button_notes.hpp"
 #include "configuration.hpp"
-#include "HX710B.h"
-
-int SCK_PIN = A7;
-int OUT_PIN = 8;
-
-HX710B air_press;
 
 const int BUTTONS_PIN[4] = { 5,4,3,2 };
 const int AIR_BUTTON_PIN = 12;
 const int LED_PIN = 13;
-const int ANALOG_PIN = A7;
-const Channel DEFAULT_CHANNEL = CHANNEL_1;
+const int ANALOG_PIN = A0;
+const Channel DEFAULT_CHANNEL = Channel_1;
 
 USBMIDI_Interface midiInterface;
 
@@ -29,10 +23,13 @@ void update_buttons_state();
 void update_velocity();
 void update_current_note();
 
-void setup() {
-  if (!air_press.init()) {
-    while(1);
+struct ConfigCallback : MIDI_Callbacks {
+  void onSysExMessage(MIDI_Interface &, SysExMessage sysex) override {
+    handle_configuration_message(sysex);
   }
+} callback {}; 
+
+void setup() {
   for(int i = 0; i < sizeof(BUTTONS_PIN) / sizeof(int); i++)
   {
     pinMode(BUTTONS_PIN[i], INPUT);
@@ -41,6 +38,7 @@ void setup() {
   pinMode(AIR_BUTTON_PIN, INPUT);
   pinMode(LED_PIN, OUTPUT);
   pinMode(ANALOG_PIN, INPUT);
+  midiInterface.setCallbacks(callback);
   midiInterface.begin();
 }
 
@@ -90,16 +88,15 @@ void update_buttons_state() {
 }
 
 void update_velocity() {
-  //int a = air_press.read();
-//  int8_t new_velocity = map(analogRead(ANALOG_PIN), 0, 1000, 0, 126);
-//  int diff = abs_diff(new_velocity, current_velocity);
-//  if(diff > MIN_VELOCITY_DIFF)
-//  {
-//    current_velocity = new_velocity;
-//    should_update_velocity = true;
-//  }
-//  else 
-//  {
-//    should_update_velocity = false;
-//  }
+  int8_t new_velocity = map(analogRead(ANALOG_PIN), 0, 1000, 0, 126);
+  int diff = abs_diff(new_velocity, current_velocity);
+  if(diff > MIN_VELOCITY_DIFF)
+  {
+    current_velocity = new_velocity;
+    should_update_velocity = true;
+  }
+  else 
+  {
+    should_update_velocity = false;
+  }
 }
